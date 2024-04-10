0725b35c109a3bc508f7a0511e4030106d4de4aa
vincedebug <155678454+vincedebug@users.noreply.github.com>
2024-04-10 10:07:03 +0800

    Add files via upload

---
+++ Lab06_Ermino/Lab06_Ermino/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Lab06_Ermino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtExtendedPrice = new System.Windows.Forms.TextBox();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.txtTotalAmount = new System.Windows.Forms.TextBox();
+            this.txtNumberTransactions = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtAmountDue = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Items:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Price:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(46, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Extended Price:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(55, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "30% Discount:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(67, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Amount Due:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(112, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 19);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Total Sales:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 75);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(198, 19);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Number of Transactions:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnCalculate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalculate.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(22, 402);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(101, 47);
+            this.btnCalculate.TabIndex = 9;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(147, 402);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(93, 47);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(266, 402);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(89, 47);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtQuantity.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.Location = new System.Drawing.Point(212, 18);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(86, 26);
+            this.txtQuantity.TabIndex = 12;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPrice.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrice.Location = new System.Drawing.Point(212, 50);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(131, 26);
+            this.txtPrice.TabIndex = 13;
+            this.txtPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(179, 89);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // txtExtendedPrice
+            // 
+            this.txtExtendedPrice.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtExtendedPrice.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtExtendedPrice.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExtendedPrice.Location = new System.Drawing.Point(183, 30);
+            this.txtExtendedPrice.Name = "txtExtendedPrice";
+            this.txtExtendedPrice.Size = new System.Drawing.Size(113, 19);
+            this.txtExtendedPrice.TabIndex = 15;
+            this.txtExtendedPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtDiscount.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDiscount.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiscount.Location = new System.Drawing.Point(183, 68);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(113, 19);
+            this.txtDiscount.TabIndex = 16;
+            this.txtDiscount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTotalAmount
+            // 
+            this.txtTotalAmount.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtTotalAmount.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTotalAmount.Location = new System.Drawing.Point(216, 32);
+            this.txtTotalAmount.Name = "txtTotalAmount";
+            this.txtTotalAmount.ReadOnly = true;
+            this.txtTotalAmount.Size = new System.Drawing.Size(127, 22);
+            this.txtTotalAmount.TabIndex = 17;
+            this.txtTotalAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtNumberTransactions
+            // 
+            this.txtNumberTransactions.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtNumberTransactions.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNumberTransactions.Location = new System.Drawing.Point(216, 68);
+            this.txtNumberTransactions.Name = "txtNumberTransactions";
+            this.txtNumberTransactions.ReadOnly = true;
+            this.txtNumberTransactions.Size = new System.Drawing.Size(127, 22);
+            this.txtNumberTransactions.TabIndex = 18;
+            this.txtNumberTransactions.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.txtPrice);
+            this.groupBox1.Controls.Add(this.txtQuantity);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(365, 86);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtAmountDue);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtExtendedPrice);
+            this.groupBox2.Controls.Add(this.txtDiscount);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 139);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(365, 138);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            // 
+            // txtAmountDue
+            // 
+            this.txtAmountDue.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtAmountDue.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtAmountDue.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmountDue.Location = new System.Drawing.Point(183, 100);
+            this.txtAmountDue.Name = "txtAmountDue";
+            this.txtAmountDue.Size = new System.Drawing.Size(113, 19);
+            this.txtAmountDue.TabIndex = 17;
+            this.txtAmountDue.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.txtNumberTransactions);
+            this.groupBox3.Controls.Add(this.txtTotalAmount);
+            this.groupBox3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(12, 283);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(365, 113);
+            this.groupBox3.TabIndex = 21;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Summary Information";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 25);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Clothing Sales by Vince Ermino";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 465);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtExtendedPrice;
+        private System.Windows.Forms.TextBox txtDiscount;
+        private System.Windows.Forms.TextBox txtTotalAmount;
+        private System.Windows.Forms.TextBox txtNumberTransactions;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAmountDue;
+    }
+}
+
---
+++ Lab06_Ermino/Lab06_Ermino/Form1.cs
@@ -0,0 +1,119 @@
+﻿/* Project:     
+ * Date: April 3, 2024
+ * Programmer: Vince Ermino
+ * Description: This project inputs sales information for clothing.
+ * It Calculates the extended price and discount for a sale.
+ * Uses variables, constants, and calculations.
+ * Note that no error trapping is included in this version 
+ * of the program
+*/
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+// 
+
+namespace Lab06_Ermino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Declare the constant
+        private const decimal DEC_DISCOUNT_RATE = 0.3M;
+        private decimal decTotalAmount;
+        private int intNumberTransactions;
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Calculate the discount and amount due.
+            // Declare the variables.
+
+            int intQuantity;
+            decimal decPrice, decExtendedPrice,
+                decDiscount, decAmountDue;
+
+            try
+            {
+                // Convert input values to numeric and assign
+                intQuantity = int.Parse(txtQuantity.Text);
+
+                try
+                {
+                    decPrice = decimal.Parse(txtPrice.Text);
+
+                    // Calculate values.
+                    decExtendedPrice = intQuantity * decPrice;
+                    decDiscount = Decimal.Round((decExtendedPrice * DEC_DISCOUNT_RATE), 2);
+                    decAmountDue = decExtendedPrice - decDiscount;
+                    decTotalAmount += decAmountDue;
+                    intNumberTransactions++;
+
+                    // Formal and Display answers.
+                    txtExtendedPrice.Text = decExtendedPrice.ToString("C");
+                    txtDiscount.Text = decDiscount.ToString("N");
+                    txtAmountDue.Text = decAmountDue.ToString("C");
+
+                    // Format and Display summary information
+                    txtTotalAmount.Text = decTotalAmount.ToString("C");
+                    txtNumberTransactions.Text  = intNumberTransactions.ToString();
+
+                }   
+
+
+                catch
+                {
+                    // Invalid Price.
+                    MessageBox.Show("Invalid Price.", "Data Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txtPrice.Focus();
+                    txtPrice.SelectAll();
+                }
+            }
+            catch
+            {
+                // Invalid quantity.
+                MessageBox.Show("Invalid quantity.", "Data Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtQuantity.Focus();
+                txtQuantity.SelectAll();
+            }
+
+            decTotalAmount = 0m;
+            intNumberTransactions = 0;
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtQuantity.Clear();
+            txtPrice.Clear();
+            txtDiscount.Clear();
+            txtExtendedPrice.Clear();
+            txtAmountDue.Clear();
+            txtQuantity.Focus();
+
+            decTotalAmount = 0m;
+            intNumberTransactions = 0; 
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // end the program.
+            this.Close();
+        }
+    }
+}
