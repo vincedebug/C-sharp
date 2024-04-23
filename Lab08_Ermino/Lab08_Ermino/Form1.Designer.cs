311873dbf6fa5a095fa3b211a254eb7bbf3438d8
vincedebug <vinceferrerasermino25@gmail.com>
2024-04-23 19:28:41 +0800

    Add files via upload

---
+++ Lab08_Ermino/Lab08_Ermino/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Lab08_Ermino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCustomerType = new System.Windows.Forms.TextBox();
+            this.txtSubTotal = new System.Windows.Forms.TextBox();
+            this.txtDiscountPercent = new System.Windows.Forms.TextBox();
+            this.txtDiscountAmount = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer type:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Subtotal:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Discount percent:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Discount amount:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 277);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(161, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Programmed By: Vince F. Ermino";
+            // 
+            // txtCustomerType
+            // 
+            this.txtCustomerType.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCustomerType.Location = new System.Drawing.Point(160, 16);
+            this.txtCustomerType.Name = "txtCustomerType";
+            this.txtCustomerType.Size = new System.Drawing.Size(100, 20);
+            this.txtCustomerType.TabIndex = 6;
+            // 
+            // txtSubTotal
+            // 
+            this.txtSubTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSubTotal.Location = new System.Drawing.Point(160, 50);
+            this.txtSubTotal.Name = "txtSubTotal";
+            this.txtSubTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtSubTotal.TabIndex = 7;
+            // 
+            // txtDiscountPercent
+            // 
+            this.txtDiscountPercent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDiscountPercent.Location = new System.Drawing.Point(160, 90);
+            this.txtDiscountPercent.Name = "txtDiscountPercent";
+            this.txtDiscountPercent.ReadOnly = true;
+            this.txtDiscountPercent.Size = new System.Drawing.Size(100, 20);
+            this.txtDiscountPercent.TabIndex = 8;
+            // 
+            // txtDiscountAmount
+            // 
+            this.txtDiscountAmount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDiscountAmount.Location = new System.Drawing.Point(160, 132);
+            this.txtDiscountAmount.Name = "txtDiscountAmount";
+            this.txtDiscountAmount.ReadOnly = true;
+            this.txtDiscountAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtDiscountAmount.TabIndex = 9;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTotal.Location = new System.Drawing.Point(160, 179);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 10;
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Location = new System.Drawing.Point(69, 232);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(75, 23);
+            this.btnCompute.TabIndex = 11;
+            this.btnCompute.Text = "Calculate";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(185, 232);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 345);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtDiscountAmount);
+            this.Controls.Add(this.txtDiscountPercent);
+            this.Controls.Add(this.txtSubTotal);
+            this.Controls.Add(this.txtCustomerType);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Invoice Total";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCustomerType;
+        private System.Windows.Forms.TextBox txtSubTotal;
+        private System.Windows.Forms.TextBox txtDiscountPercent;
+        private System.Windows.Forms.TextBox txtDiscountAmount;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Lab08_Ermino/Lab08_Ermino/Form1.cs
@@ -0,0 +1,104 @@
+﻿/*Project Name: Invoice Calculator
+ * Developed by: Vince Ferreras Ermino
+ * Date Accomplished: 4/21/2024
+ * Description: This is a simple Invoice calculator 
+ * for people running their own business 
+ * or working in a financial or accounting department 
+ * It displays the Discount Percent and discount amount and the total amount. 
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab08_Ermino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            string strCustomerType;
+            string strMessage;
+            decimal decSubTotal;
+            decimal decDiscountPercent = .0m;
+
+            if (txtCustomerType.Text == string.Empty)
+            {
+                MessageBox.Show("Invalid Customer Type. Customer Type must not be blank",
+                    "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }// end if.
+
+            if (txtSubTotal.Text != "") // Not blank.
+            {
+                try
+                {
+                    strCustomerType = txtCustomerType.Text;
+
+                    decSubTotal = decimal.Parse(txtSubTotal.Text);
+
+                    if (strCustomerType == "R")
+                    {
+                        if (decSubTotal < 100)
+                            decDiscountPercent = .0m;
+                        else if (decSubTotal >= 100 && decSubTotal < 250)
+                            decDiscountPercent = .1m;
+                        else if (decSubTotal >= 250)
+                            decDiscountPercent = .25m;
+                    } // end if
+                    else if (strCustomerType == "C")
+                    {
+                        if (decSubTotal < 250)
+                            decDiscountPercent = .2m;
+                        else
+                            decDiscountPercent = .3m;
+                    } // end else if
+                    else
+                    {
+                        decDiscountPercent = .4m;
+                    } // end else
+
+                    decimal discountAmount = decSubTotal * decDiscountPercent;
+                    decimal invoiceTotal = decSubTotal - discountAmount;
+
+                    txtDiscountPercent.Text = decDiscountPercent.ToString("p1");
+                    txtDiscountAmount.Text = discountAmount.ToString("c");
+                    txtTotal.Text = invoiceTotal.ToString("c");
+
+                    txtCustomerType.Focus();
+                } // end try
+                catch // Nonnumeric data.
+                {
+                    strMessage = "Nonnumeric data entered for sub total.";
+                    MessageBox.Show(strMessage, "Data Entry Error");
+                } // end catch
+            } // end if 
+            else // for missing data.
+            {
+                strMessage = "The quantity is required. ";
+                MessageBox.Show(strMessage, "Data entry error");
+            } // end else
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+
+
